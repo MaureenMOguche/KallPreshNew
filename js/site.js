72fa0faa4e4cc4f856054cff30f858a865dc8891
MaureenMOguche <career.oguchemaureen@outlook.com>
2023-01-22 01:42:44 +0100

    kallypresh

---
+++ js/site.js
@@ -0,0 +1,11 @@
+﻿$(window).on('load', function(){
+    $('#status').fadeOut();
+    $('#preloader').delay(250).fadeOut();
+})
+
+$(function (){
+    $('.home-inner img:gt(0)').hide();
+    setInterval(function (){
+        $('.home-inner :first-child').fadeOut().next('img').fadeIn().end().appendTo('.home-inner');
+    }, 4000);
+})
